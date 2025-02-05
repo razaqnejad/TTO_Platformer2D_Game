1aca2c48106a2b32566740be512614014d0656ad
razaqnejad <razaqnejad@gmail.com>
2025-02-05 22:50:06 +0330

    Publish

---
+++ Assets/Scripts/Core/CameraController.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //Room camera
+    [SerializeField] private float speed;
+    private float currentPosX;
+    private Vector3 velocity = Vector3.zero;
+
+    //Follow player
+    [SerializeField] private Transform player;
+    [SerializeField] private float aheadDistance_X;
+    [SerializeField] private float aheadDistance_Y;
+    [SerializeField] private float cameraSpeed;
+    private float lookAhead_X;
+    private float lookAhead_Y;
+
+    private void Update()
+    {
+        //Room camera
+        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), ref velocity, speed);
+
+        //Follow player
+        transform.position = new Vector3(player.position.x + lookAhead_X, player.position.y + lookAhead_Y, transform.position.z);
+        lookAhead_X = Mathf.Lerp(lookAhead_X, (aheadDistance_X * player.localScale.x), Time.deltaTime * cameraSpeed);
+        lookAhead_Y = Mathf.Lerp(lookAhead_Y, (aheadDistance_Y * player.localScale.x), Time.deltaTime * cameraSpeed);
+    }
+
+    public void MoveToNewRoom(Transform _newRoom)
+    {
+        currentPosX = _newRoom.position.x;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Through The Ordinary
 ### 2D Platformer Game
+
+### Installation Files
+Download the whole folder as a .zip file and run "Throug The Ordinary.exe"
+
+### Assest
+Folder Includes all the assets of the project although they are not enough to open the project on unity
